a10de698e136f9139bd2874031e655556b41e144
Claudia Villarroel <claudia.villarroel@live.com>
2024-02-16 17:02:16 -0300

    :hammer: Estructura backend en proceso

---
+++ BackEnd/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const routes = require('./routes/index');
+
+//middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+//error handler
+app.use((err, req, res, next) => {
+  res.status(500).send({ error: err.message });
+});
+
+//routes
+app.use('/', routes);
+
+module.exports = app;
